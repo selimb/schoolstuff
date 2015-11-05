246b6e039425af36754b66a21603d47b1d1a37db
Selim Belhaouane <selim.belhaouane@gmail.com>
2015-11-05 17:57:40 -0500

    Work on math578 numerical questions

---
+++ math578/hw4/q11.m
@@ -0,0 +1,36 @@
+%%
+% Input data
+n=10;
+m=40;
+randn('state',0);
+u = linspace(-1,1,m);
+v = 1./(5+40*u.^2) + 0.1*u.^3 + 0.01*randn(1,m);
+A = vander(u');
+A = A(:, m - n + [1:n]);
+
+norms = [1, 1.5, 2, 2.5, 4, 8, inf];
+l = length(norms);
+max_residuals = zeros(1, l);
+
+%%
+% Construct polynomial for each norm and evaluate maximum norm of residual.
+
+for i = 1 : length(norms)
+    cvx_begin quiet
+        variable x(n)
+        minimize (norm(A*x - v', norms(i)))
+    cvx_end
+    vpol = x(1)*ones(1, length(u));
+    for j = 2:n
+      vpol = vpol.*u + x(j);
+    end;
+    max_residuals(i) = max(vpol - v);
+end
+
+%%
+% Normalize and plot
+max_residuals = max_residuals/max_residuals(l);
+
+plot(norms(1:l-1), max_residuals(1:l-1), '-o')
+xlabel('$p$', 'interpreter', 'latex')
+ylabel('$\hat{r}_p$', 'interpreter', 'latex')
