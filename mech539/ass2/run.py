6a4fffe5aacf718cea4cf5c557d2d4a40cc5cbd4
Selim Belhaouane <selim.belhaouane@gmail.com>
2016-02-14 23:23:47 -0500

    progress

--- mech539/ass2/run.py
+++ mech539/ass2/run.py
@@ -1,6 +1,7 @@
 import numpy as np
 from glob import glob
 import os
+from pprint import pformat
 import shutil
 import subprocess
 
@@ -31,6 +32,37 @@ def move_dat(prefix):
         newpath = os.path.join('dat', newfilename)
         shutil.move(oldpath, newpath)
 
+def get_time_taken():
+    timefile = 't.dat'
+    times = open(timefile).readlines()
+    times = list(times)
+    iters = len(times)
+    abs_time_taken = float(times[-1].strip())
+    time_per_iter = abs_time_taken/iters
+    return iters, time_per_iter, abs_time_taken
+
+def compile(olevel):
+    subprocess.call("cd src; make compile olevel=%s" % olevel, shell=True)
+
+def time_analysis():
+    olevels = ['-g', '-O1', '-O2', '-O3']
+    solvers = [1, 2] # Jacobi and Gauss
+    NXs = [100,200,400]
+    times = {}
+    for olevel in olevels:
+        times[olevel] = {}
+        print('Compiling with %s' % olevel)
+        compile(olevel)
+        for solver in solvers:
+            times[olevel][solver] = {}
+            for nx in NXs:
+                modify_param('solverID', solver)
+                modify_param('nx', nx)
+                prefix = '%s_%i' % (solver, nx)
+                print('Running %s' % prefix)
+                print(run())
+                times[olevel][solver][nx] = get_time_taken()
+    open('time_data.py', 'w').write(pformat(times))
 
 def run_all():
     print('Running all cases')
@@ -45,4 +77,5 @@ def run_all():
             move_dat(prefix)
     print('Done')
 if __name__ == '__main__':
+    # time_analysis()
     run_all()
