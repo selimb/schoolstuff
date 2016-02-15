cbb731910d0e4dfb2958f6d757e4362230f95178
Selim Belhaouane <selim.belhaouane@gmail.com>
2016-02-15 17:24:29 -0500

    use latex font and reduce label font size

--- matplotlibrc.py
+++ matplotlibrc.py
@@ -5,11 +5,15 @@
 import matplotlib
 
 params={
-    'axes.labelsize' : 18,
+    'backend': 'ps',
+    'axes.labelsize' : 15,
     'axes.titlesize' : 18,
     'legend.fontsize' : 14,
     'legend.fancybox' : True,
     'legend.framealpha' : 0.5,
     'legend.loc' : 'best',
+    'text.latex.preamble': [r'\usepackage{gensymb}'],
+    'text.usetex': True,
+    'font.family': 'serif'
 }
 matplotlib.rcParams.update(params)
