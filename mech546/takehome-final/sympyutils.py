0641f1d2d2db3069058e564fd6f7c542d0c2b4e4
Selim Belhaouane <selim.belhaouane@gmail.com>
2016-04-26 12:59:27 -0400

    takehome

---
+++ mech546/takehome-final/sympyutils.py
@@ -0,0 +1,27 @@
+import sympy as sp
+import IPython.display as disp
+
+def mk_expr(expr, name, do_align=False):
+    eq = '='
+    if do_align:
+        eq = '&' + eq
+
+    return name + eq + sp.latex(expr)
+
+def mk_align(body):
+    return '\n'.join([r'\begin{aligned}', body, r'\end{aligned}'])
+
+def show(expr, name):
+    return disp.Math(mk_expr(expr, name))
+
+def show_add(exprs, name):
+    body = name + '=&' + r'\\ &+ '.join([sp.latex(expr) for expr in exprs])
+    return disp.Math(mk_align(body))
+
+def show_list(exprs, name):
+    names = ['%s_%i' % (name, i+1) for i in range(len(exprs))]
+    assert len(exprs) == len(names)
+    body = r'\\'.join(
+        [mk_expr(expr.simplify(), name, True) for expr, name in zip(exprs, names)]
+    )
+    return disp.Math(mk_align(body))
